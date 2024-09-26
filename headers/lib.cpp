dc0a728294bf1ed6f01dd23bed48d475b2e6d12e
Mura <ope_overlord@hotmail.com>
2024-09-26 18:58:31 +0900

    adds c++features macros in each header

--- headers/lib.cpp
+++ headers/lib.cpp
@@ -7,22 +7,30 @@
 #include "new"
 namespace std {
 
-nothrow_t const 	nothrow;
+nothrow_t const nothrow;
 
 static new_handler new_handler_s;
-new_handler get_new_handler() noexcept { return new_handler_s; }
-new_handler set_new_handler(new_handler _new_p) noexcept { auto p = new_handler_s; new_handler_s = _new_p; return p; }
+new_handler		   get_new_handler() noexcept { return new_handler_s; }
+new_handler		   set_new_handler(new_handler _new_p) noexcept {
+		   auto p		 = new_handler_s;
+		   new_handler_s = _new_p;
+		   return p;
+}
 
-}	// namespace std
+}	 // namespace std
 // ===========================================================================
 #include "exception"
 namespace std {
 
 static terminate_handler terminate_handler_s;
-terminate_handler get_terminate() noexcept { return terminate_handler_s; }
-terminate_handler set_terminate(terminate_handler _f) noexcept { auto f = terminate_handler_s; terminate_handler_s = _f; return f; }
+terminate_handler		 get_terminate() noexcept { return terminate_handler_s; }
+terminate_handler		 set_terminate(terminate_handler _f) noexcept {
+		   auto f			   = terminate_handler_s;
+		   terminate_handler_s = _f;
+		   return f;
+}
 
-}	// namespace std
+}	 // namespace std
 // ===========================================================================
 #include "typeinfo"
 namespace std {
@@ -30,12 +38,12 @@ namespace std {
 class type_info {
 public:
 	virtual ~type_info() {}
-	bool operator==(type_info const& _rhs) const noexcept;
-	bool before(type_info const& _rhs) const noexcept;
-	size_t hash_code() const noexcept;
+	bool		operator==(type_info const& _rhs) const noexcept;
+	bool		before(type_info const& _rhs) const noexcept;
+	size_t		hash_code() const noexcept;
 	char const* name() const noexcept;
-	type_info(type_info const&) = delete;
-	type_info& operator =(type_info const&) = delete;
+	type_info(type_info const&)			   = delete;
+	type_info& operator=(type_info const&) = delete;
 };
 
 class bad_cast : public exception {
@@ -48,21 +56,21 @@ public:
 	char const* what() const noexcept override { return "bad typeid"; }
 };
 
-}	// namespace std
+}	 // namespace std
 // ===========================================================================
 
+#include "initializer_list"
+#include "type_traits"
 #include "atomic"
+#include "bit"
 #include "cfloat"
 #include "climits"
+#include "compare"
+#include "concepts"
+#include "coroutine"
 #include "cstdarg"
 #include "cstddef"
 #include "cstdint"
 #include "cstdlib"
-#include "initializer_list"
 #include "limits"
-#include "type_traits"
 #include "version"
-#include "compare"
-#include "concepts"
-#include "coroutine"
-#include "bit"
